11cce02d7af93906fbfecf1135d58c6e8cfaaea3
SandraBayabos <sandra.bayabos@gmail.com>
2019-04-30 13:15:50 +0800

    refactored templates to use a master layout

--- server.py
+++ server.py
@@ -7,21 +7,29 @@ app = Flask(__name__)
 #     name = name.upper()
 #     return render_template('index.html', name=name)
 
-
-@app.route("/")
-def index1():
-    signed_in = False
-    return render_template('index.html', signed_in=signed_in)
-
-
-@app.route("/another")
-def show():
-    return '<h1>Yo</h1>'
+# @app.route("/")
+# def index1():
+#     signed_in = False
+#     return render_template('index.html', signed_in=signed_in)
 
 
-@app.route('/user/<username>')
-def username(username):
-    return f'Hi {username}'
+# @app.route("/another")
+# def show():
+#     return '<h1>Yo</h1>'
+
+
+# @app.route('/user/<username>')
+# def username(username):
+#     return f'Hi {username}'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
